34f1d2bb6c027f58e7b5b6a738fa5cfd9d91629a
rashed <rashedulislam.shajol@gmail.com>
2026-05-17 09:56:40 +0600

    balance

---
+++ balance.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main() 
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    string str;
+	    cin>>str;
+	    
+	    if(str[0]!=str.back())
+	    str[0]=str.back();
+	    
+	    cout<<str<<endl;
+	}
+	return 0;
+}
