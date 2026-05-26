3d67247d6371aa4ca0fdc6f9bcf32495ed8961f8
rashed <rashedulislam.shajol@gmail.com>
2026-05-26 16:01:27 +0600

    update

--- median.cpp
+++ median.cpp
@@ -9,17 +9,29 @@ int main()
     fastread();
     ll t,n,k,a;
     cin>>t;
-    while(t--){
+
+
+    while(t--)
+    
+    {
         vector<ll>v;
         cin>>n>>k;
+
         ll range = n*k,j = 0,div = 0,sum = 0;
         div = n/2;
 
-        for(ll i=0; i<range; i++){
+
+        for(ll i=0; i<range; i++)
+        {
             cin>>a;
             v.pb(a);
         }
-        for(ll i=range-1-div; i>=0; i-=div+1){
+
+
+        
+        
+        for(ll i=range-1-div; i>=0; i-=div+1)
+        {
          
             sum += v[i];
             j++;
